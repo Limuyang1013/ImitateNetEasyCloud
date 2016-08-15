5597f0e69542f2ffee9605d6e034d6f559dfaf24
li muyang <lin794653318@gmail.com>
2016-08-15 17:38:31 +0800

    修改启动动画

--- app/src/main/java/com/stest/neteasycloud/BaseActivity.java
+++ app/src/main/java/com/stest/neteasycloud/BaseActivity.java
@@ -1,9 +1,23 @@
 package com.stest.neteasycloud;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Limuyang on 2016/8/15.
  */
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initViews();
+    }
+
+    /**
+     * 初始化控件
+     */
+    public abstract void initViews();
+
 }
