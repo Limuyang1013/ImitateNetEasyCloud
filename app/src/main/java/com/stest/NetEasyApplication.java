9ba06dd443bf72caad4bc8534fd4c55360414652
li muyang <lin794653318@gmail.com>
2016-08-17 11:17:58 +0800

    添加一些布局

--- app/src/main/java/com/stest/NetEasyApplication.java
+++ app/src/main/java/com/stest/NetEasyApplication.java
@@ -2,10 +2,16 @@ package com.stest;
 
 import android.app.Application;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 /**
  * Created by Limuyang on 2016/8/15.
  */
 public class NetEasyApplication extends Application {
 
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(this);
+    }
 }
