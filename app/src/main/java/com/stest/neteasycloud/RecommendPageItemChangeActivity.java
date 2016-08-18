947d07fbdd806b451476f0cdb48c690a8db432cd
li muyang <lin794653318@gmail.com>
2016-08-18 17:07:59 +0800

    添加拖拽列表文字，待完善布局以及数据的持久化保存

--- app/src/main/java/com/stest/neteasycloud/RecommendPageItemChangeActivity.java
+++ app/src/main/java/com/stest/neteasycloud/RecommendPageItemChangeActivity.java
@@ -3,6 +3,7 @@ package com.stest.neteasycloud;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.view.ViewStub;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
@@ -33,6 +35,8 @@ public class RecommendPageItemChangeActivity extends AppCompatActivity {
     private Toolbar toolbar;
     @ViewInject(R.id.drag_lv)
     private DragSortListView drag_lv;
+    @ViewInject(R.id.drag_restore_txt)
+    private TextView drag_txt;
     //适配器
     private ArrayAdapter<String> mAdapter;
     private ActionBar actionBar;
@@ -72,6 +76,7 @@ public class RecommendPageItemChangeActivity extends AppCompatActivity {
         drag_lv.setRemoveListener(onRemove);
         //显示顶部横线
         drag_lv.addHeaderView(new ViewStub(this));
+        drag_lv.addFooterView(new ViewStub(this));
 
         DragSortController controller = new DragSortController(drag_lv);
         controller.setDragHandleId(R.id.move);
@@ -88,6 +93,9 @@ public class RecommendPageItemChangeActivity extends AppCompatActivity {
         SimpleFloatViewManager simpleFloatViewManager = new SimpleFloatViewManager(drag_lv);
         simpleFloatViewManager.setBackgroundColor(Color.TRANSPARENT);
         drag_lv.setFloatViewManager(simpleFloatViewManager);
+
+        //添加下划线
+        drag_txt.getPaint().setFlags(Paint.UNDERLINE_TEXT_FLAG);
     }
 
     public static void start(Context context) {
