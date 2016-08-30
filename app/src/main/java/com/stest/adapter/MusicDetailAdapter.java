556d76671443e3fe93643c3ca5c125cce30cc229
li muyang <lin794653318@gmail.com>
2016-08-30 17:32:14 +0800

    增加布局

--- app/src/main/java/com/stest/adapter/MusicDetailAdapter.java
+++ app/src/main/java/com/stest/adapter/MusicDetailAdapter.java
@@ -16,8 +16,8 @@ public class MusicDetailAdapter extends SuperAdapter<String> {
 
     private int[] icn;
 
-    public MusicDetailAdapter(Context context, List<String> items, int layoutResId) {
-        super(context, items, layoutResId);
+    public MusicDetailAdapter(Context context, List<String> items, int layoutResIde) {
+        super(context, items, layoutResIde);
         icn = new int[]{R.drawable.music_icn_local, R.drawable.music_icn_recent, R.drawable.music_icn_dld,
                 R.drawable.music_icn_artist, R.drawable.music_icn_dj, R.drawable.music_icn_mv};
     }
@@ -28,8 +28,6 @@ public class MusicDetailAdapter extends SuperAdapter<String> {
         holder.setText(R.id.detail_title, item);
         holder.setText(R.id.detail_number, getContext().getResources().getString(R.string.detail_txt));
         holder.setImageResource(R.id.detail_icn, icn[layoutPosition]);
-
     }
-
-
 }
+
