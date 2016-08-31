89fb244d3c9eb90446aef6b72f6d9d9f459cf5c8
li muyang <lin794653318@gmail.com>
2016-08-31 16:51:34 +0800

    更新界面

--- app/src/main/java/com/stest/InnerFragment/SinglesFragment.java
+++ app/src/main/java/com/stest/InnerFragment/SinglesFragment.java
@@ -5,13 +5,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
 import com.stest.neteasycloud.R;
 
 // 单曲
-public class SinglesFragment extends Fragment {
+public class SinglesFragment extends Fragment implements View.OnClickListener {
 
+    @ViewInject(R.id.play_all_layout)
+    private RelativeLayout play_layout;
+    @ViewInject(R.id.play_all_select)
+    private ImageView select;
     private View v;
 
     @Override
@@ -21,7 +28,24 @@ public class SinglesFragment extends Fragment {
         }
         View v = inflater.inflate(R.layout.fragment_singles, container, false);
         ViewUtils.inject(this, v);
+        initWidgets();
         return v;
     }
 
+    private void initWidgets() {
+        play_layout.setOnClickListener(this);
+        select.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            //播放全部
+            case R.id.play_all_layout:
+                break;
+            //选择操作
+            case R.id.play_all_select:
+                break;
+        }
+    }
 }
