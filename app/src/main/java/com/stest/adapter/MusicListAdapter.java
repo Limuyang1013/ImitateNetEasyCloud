8e347376c3a2f3b1c58a95ce0716af03ff7f3c12
li muyang <lin794653318@gmail.com>
2016-09-01 16:41:41 +0800

    扫描本地音乐数据

--- app/src/main/java/com/stest/adapter/MusicListAdapter.java
+++ app/src/main/java/com/stest/adapter/MusicListAdapter.java
@@ -1,6 +1,8 @@
 package com.stest.adapter;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.stest.model.MusicInfoDetail;
 import com.stest.neteasycloud.R;
@@ -26,8 +28,20 @@ public class MusicListAdapter extends SuperAdapter<MusicInfoDetail> {
         holder.setText(R.id.song_title, item.getTitle());
         holder.setText(R.id.song_artist, item.getArtist());
         holder.setText(R.id.song_albumn, item.getAlbum());
-        holder.setImageResource(R.id.dld_ok,R.drawable.list_icn_dld_ok);
-        holder.setImageResource(R.id.hq,R.drawable.list_icn_hq_sml);
-        holder.setImageResource(R.id.popup_menu,R.drawable.list_icn_more);
+        holder.setImageResource(R.id.dld_ok, R.drawable.list_icn_dld_ok);
+        holder.setImageResource(R.id.hq, R.drawable.list_icn_hq_sml);
+        holder.setImageResource(R.id.popup_menu, R.drawable.list_icn_more);
+
+    }
+
+    @Override
+    public SuperViewHolder onCreate(View convertView, ViewGroup parent, int viewType) {
+        SuperViewHolder holder = super.onCreate(convertView, parent, viewType);
+        holder.findViewById(R.id.popup_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+        return holder;
     }
 }
