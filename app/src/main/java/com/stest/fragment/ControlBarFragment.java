65f34a7c0e0217bd3bdd3099a4fc399186ca2235
lin794653318@gmail.com <794653318>
2016-09-08 16:42:14 +0800

    把音乐数据存入本地数据库

---
+++ app/src/main/java/com/stest/fragment/ControlBarFragment.java
@@ -0,0 +1,86 @@
+package com.stest.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
+import com.stest.neteasycloud.R;
+
+/**
+ * Created by Limuyang on 2016/9/8.
+ * 底部控制栏
+ */
+
+public class ControlBarFragment extends Fragment implements View.OnClickListener {
+    private View v;
+    @ViewInject(R.id.progress)
+    private ProgressBar mProgress;
+    @ViewInject(R.id.music_control_layout)
+    private LinearLayout control_layout;
+    @ViewInject(R.id.albumn_pic)
+    private SimpleDraweeView albumn;
+    @ViewInject(R.id.song)
+    private TextView song_txt;
+    @ViewInject(R.id.singer)
+    private TextView singer_txt;
+    @ViewInject(R.id.playlist_btn)
+    private ImageView playlist;
+    @ViewInject(R.id.play_btn)
+    private ImageView play;
+    @ViewInject(R.id.next_btn)
+    private ImageView next;
+
+    public static ControlBarFragment newInstance() {
+        return new ControlBarFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (v != null) {
+            ViewUtils.inject(this, v);
+            return v;
+        }
+        v = inflater.inflate(R.layout.bottom_music_layout, container, false);
+        ViewUtils.inject(this, v);
+        addView();
+        return v;
+    }
+
+    private void addView() {
+        control_layout.setOnClickListener(this);
+        playlist.setOnClickListener(this);
+        play.setOnClickListener(this);
+        next.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            //整个底部播放栏布局
+            case R.id.music_control_layout:
+                break;
+            //播放列表
+            case R.id.playlist_btn:
+                break;
+            //播放控制
+            case R.id.play_btn:
+                break;
+            //下一曲
+            case R.id.next_btn:
+                break;
+        }
+
+    }
+}
